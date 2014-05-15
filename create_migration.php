b6996822fcad8f45ac97cd327bfbfd5cc89c4dca
Fernando L. Canizo (aka conan) <conan@muriandre.com>
2014-05-15 01:02:16 -0300

    Refactor migration creator
    - set default timezone 'UTC'
    - use a more legible timestamp
    - improve code readability

--- create_migration.php
+++ create_migration.php
@@ -1,27 +1,38 @@
 #!/usr/bin/php
 <?php
-	if(isset($argv[1]) && $argv[1]!='--help' && $argv[1]!='-h') {
-		$ext = "sql";
-		if(isset($argv[2])) {
-			$ext = $argv[2];
-			if($ext != "sql" && $ext != "php") {
-				die("Unknown extention $ext\n");
-			}
-		}
-		date_default_timezone_set("Europe/Stockholm");
-		$filename=date("YmdHis")."_".$argv[1].".$ext";
-		$f = fopen(dirname($argv[0]) . "/$filename",'w');
-		if($ext == "php") {
-			fwrite($f, "<?php
-/*
- * Please be verbose. Use echo and end lines with \\n
- * Use migration_sql(\$query) to run queries (prints and runs the query)
- */
+
+define('DS', DIRECTORY_SEPARATOR);
+
+if(isset($argv[1]) && $argv[1] != '--help' && $argv[1] != '-h'):
+	$thisScript = $argv[0];
+	$path = dirname($thisScript);
+	$migrationName = $argv[1];
+	$ext = 'sql'; // default to SQL commands
+
+	if(isset($argv[2])):
+		$ext = $argv[2];
+
+		if($ext != "sql" && $ext != "php"):
+			die("Unknown extention $ext" . PHP_EOL);
+		endif;
+	endif;
+
+	date_default_timezone_set('UTC');
+	$filename = $path . DS . date('Y.m.d-H.i.s') . "_" . $migrationName . ".$ext";
+	$f = fopen($filename, 'w');
+
+	if('php' === $ext):
+		fwrite($f, "<?php
+
+// Please be verbose. Use echo and end lines with \\n
+// Use migration_sql(\$query) to run queries (prints and runs the query)
+
 ?>");
-		}
-		fclose($f);
-		echo "Created migration ".dirname($argv[0])."/$filename\n";
-	} else {
-		echo "Usage: ./create_migration name [*sql|php]\n";
-	}
-?>
+	endif;
+	fclose($f);
+
+	echo "Created migration $filename", PHP_EOL;
+
+else:
+	echo "Usage: $thisScript name [*sql|php]", PHP_EOL;
+endif;
